99af501a2fd46987cc1092483b0088a12217c645
kenyonshutt <kenyonshutt@gmail.com>
2025-07-25 00:40:10 -0700

    added to-do comments to formboard; consider splitting into formboard + pdf generator

--- artifact_builders/standard_harnice_formboard/standard_harnice_formboard-rev1/standard_harnice_formboard.py
+++ artifact_builders/standard_harnice_formboard/standard_harnice_formboard-rev1/standard_harnice_formboard.py
@@ -1,23 +1,25 @@
+from harnice import svg_outputs, flagnotes, rev_history, svg_utils, instances_list
+
 #=============== REBUILDING OUTPUT SVG #===============
 # ensure page setup is defined, if not, make a basic one
 page_setup_contents = svg_outputs.update_page_setup_json()
 
 revinfo = rev_history.revision_info()
 
-# add parent types to make filtering easier
-instances_list.add_parent_instance_type()
-
 # prepare the building blocks as svgsflagnotes.make_note_drawings()
 flagnotes.make_leader_drawings()
-svg_outputs.prep_formboard_drawings(page_setup_contents)
-svg_outputs.prep_bom()
-svg_outputs.prep_buildnotes_table()
-svg_outputs.prep_revision_table()
-# esch done under run_wireviz.generate_esch()
 
-svg_outputs.prep_tblocks(page_setup_contents, revinfo)
+#TODO: uncomment this per https://github.com/kenyonshutt/harnice/issues/217
+#svg_outputs.prep_formboard_drawings(page_setup_contents)
+#svg_outputs.prep_buildnotes_table()
 
-svg_outputs.prep_master(page_setup_contents)
+#TODO: add bom processor to svgoutputs https://github.com/kenyonshutt/harnice/issues/226
+#svg_outputs.prep_bom()
+#svg_outputs.prep_revision_table()
+
+#svg_outputs.prep_tblocks(page_setup_contents, revinfo)
+
+#svg_outputs.prep_master(page_setup_contents)
     # merges all building blocks into one main support_do_not_edit master svg file
 
 svg_outputs.update_harnice_output(page_setup_contents)
