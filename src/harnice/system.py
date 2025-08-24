080d290b99cb94e59bf416af256c0fc2d0e11cab
kenyonshutt <kenyonshutt@gmail.com>
2025-08-24 00:13:51 -0700

    blank channel map is generated but connections are not assigned yet

--- src/harnice/system.py
+++ src/harnice/system.py
@@ -2,6 +2,7 @@ from harnice import fileio, component_library
 import runpy
 import os
 import csv
+import json
 
 CHANNEL_MAP_COLUMNS = [
     "connected_connector_group",
@@ -20,6 +21,7 @@ from harnice import featuretree, system
 featuretree.runprebuilder("kicad_pro_to_netlist_prebuilder", "public")
 featuretree.runprebuilder("kicad_pro_to_bom_prebuilder", "public")
 system.pull_boxes_from_library()
+system.new_channel_map()
 """
 
 def render():
@@ -59,3 +61,65 @@ def pull_boxes_from_library():
                 quiet=False
             )
         imported_boxes.append(refdes)
+
+def read_signals_list(path):
+    with open(path, "r", encoding="utf-8") as f:
+        reader = csv.DictReader(f, delimiter="\t")
+        return list(reader)
+
+def read_netlist():
+    with open(fileio.path("netlist"), "r", encoding="utf-8") as f:
+        return json.load(f)
+
+def new_channel_map():
+    channel_map = []
+    seen = set()  # track unique rows by tuple key
+    netlist = read_netlist()   # load once
+
+    for refdes in read_bom_rows():
+        box_ref = refdes.get("box_ref_des")
+        if not box_ref:
+            continue
+
+        signals_path = os.path.join(
+            fileio.dirpath("imported_boxes"),
+            box_ref,
+            f"{refdes.get('MPN')}-{refdes.get('rev')}-signals-list.tsv"
+        )
+        if not os.path.exists(signals_path):
+            continue
+
+        signals = read_signals_list(signals_path)
+        if not signals:
+            continue
+
+        for signal in signals:
+            channel_id = signal.get("channel", "").strip()
+            if not channel_id:
+                continue
+
+            connector_name_of_channel = f"{box_ref}:{signal.get('connector_name', '').strip()}"
+            connected_connector_group = next(
+                (net for net, conns in netlist.items() if connector_name_of_channel in conns),
+                None
+            )
+
+            row = {
+                "connected_connector_group": connected_connector_group,
+                "from_box_refdes": box_ref,
+                "from_box_channel_id": channel_id
+            }
+
+            # create a uniqueness key from row values
+            key = (row["connected_connector_group"], row["from_box_refdes"], row["from_box_channel_id"])
+
+            if key not in seen:
+                channel_map.append(row)
+                seen.add(key)
+
+    with open(fileio.path("channel map"), "w", newline="", encoding="utf-8") as f:
+        writer = csv.DictWriter(f, fieldnames=CHANNEL_MAP_COLUMNS, delimiter="\t")
+        writer.writeheader()
+        writer.writerows(channel_map)
+
+
