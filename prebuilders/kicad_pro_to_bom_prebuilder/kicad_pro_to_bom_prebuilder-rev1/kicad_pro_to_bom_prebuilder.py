c6682b6592d4ec18678d5da97b70ed4c6764ce67
kenyonshutt <kenyonshutt@gmail.com>
2025-08-23 23:10:26 -0700

    added rev to bom

--- prebuilders/kicad_pro_to_bom_prebuilder/kicad_pro_to_bom_prebuilder-rev1/kicad_pro_to_bom_prebuilder.py
+++ prebuilders/kicad_pro_to_bom_prebuilder/kicad_pro_to_bom_prebuilder-rev1/kicad_pro_to_bom_prebuilder.py
@@ -5,10 +5,10 @@ from harnice import fileio
 prebuilder_mpn = "kicad_pro_to_bom"
 
 # kicad headers (fields in KiCad schematic, including custom attributes)
-BOM_FIELDS = ["Reference", "MFG", "MPN", "supplier", "supplier_subpath"]
+BOM_FIELDS = ["Reference", "MFG", "MPN", "supplier", "supplier_subpath", "rev"]
 
 # output headers (labels in TSV)
-BOM_LABELS = ["box_ref_des", "MFG", "MPN", "supplier", "supplier_subpath"]
+BOM_LABELS = ["box_ref_des", "MFG", "MPN", "supplier", "supplier_subpath", "rev"]
 
 def path(target_value: str) -> str:
     if target_value == "kicad sch":
