753b1102f51aac9ff5848802f650517f7677408f
kenyonshutt <kenyonshutt@gmail.com>
2025-10-14 21:20:19 -0700

    added circuit_instance.py

--- src/harnice/formboard_utils.py
+++ src/harnice/formboard_utils.py
@@ -4,7 +4,7 @@ import math
 import csv
 from PIL import Image, ImageDraw, ImageFont
 from collections import defaultdict, deque
-from harnice import instances_list, fileio
+from harnice import instances_list, fileio, circuit_instance
 
 FORMBOARD_TSV_COLUMNS = [
     "segment_id",
@@ -513,7 +513,7 @@ def map_instance_to_segments(instance_name):
         )
 
     # Find terminal nodes from the ports
-    prev_instance, next_instance = instances_list.instance_names_of_adjacent_ports(
+    prev_instance, next_instance = circuit_instance.instance_names_of_adjacent_ports( # TODO: maybe make this an argument to this function
         instance_name
     )
     node_of_prev_instance = instances_list.instance_in_cluster_with_suffix(
--- src/harnice/instances_list.py
+++ src/harnice/instances_list.py
@@ -1,5 +1,4 @@
 import csv
-from itertools import product
 import os
 import inspect
 from harnice import fileio
@@ -202,39 +201,6 @@ def add_revhistory_of_imported_part(instance_name, rev_data):
         writer = csv.DictWriter(f, fieldnames=fieldnames, delimiter="\t")
         writer.writeheader()
         writer.writerows(rows)
-
-
-def instance_names_of_adjacent_ports(target_instance):
-    for instance in read_instance_rows():
-        if instance.get("instance_name") == target_instance:
-            # assign parents to contacts based on the assumption that one of the two adjacent items in the circuit will be a node-item
-            if (
-                instance.get("circuit_id") == ""
-                or instance.get("circuit_port_number") == ""
-            ):
-                raise ValueError(f"Circuit order unspecified for {target_instance}")
-
-            circuit_id = instance.get("circuit_id")
-            circuit_port_number = int(instance.get("circuit_port_number"))
-
-            # find the adjacent port
-            prev_port = ""
-            next_port = ""
-
-            for instance2 in read_instance_rows():
-                if instance2.get("circuit_id") == circuit_id:
-                    if (
-                        int(instance2.get("circuit_port_number"))
-                        == circuit_port_number - 1
-                    ):
-                        prev_port = instance2.get("instance_name")
-                    if (
-                        int(instance2.get("circuit_port_number"))
-                        == circuit_port_number + 1
-                    ):
-                        next_port = instance2.get("instance_name")
-
-            return prev_port, next_port
 
 
 def attribute_of(target_instance, attribute):
