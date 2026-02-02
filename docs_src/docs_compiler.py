bf94b0bb422e42b5b3c8b7507f1f623277486de6
kenyonshutt <kenyonshutt@gmail.com>
2026-02-01 17:43:10 -0800

    fixed broken path

--- docs_src/docs_compiler.py
+++ docs_src/docs_compiler.py
@@ -1,12 +1,10 @@
 import commands
-import getting_started
 import interacting_with_data
 import products
 import fragments
 
 if __name__ == "__main__":
     commands.main()
-    getting_started.main()
     interacting_with_data.main()
     products.main()
     fragments.main()
