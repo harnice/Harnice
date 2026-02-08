5d45fcd8ea67ee071912cf7d210614d923aa52ed
kenyonshutt <kenyonshutt@gmail.com>
2026-02-08 09:15:06 -0800

    midway through updates

--- docs_src/fragments.py
+++ docs_src/fragments.py
@@ -1,5 +1,7 @@
 import docs_functions
-from harnice.products import harness, system
+from harnice.products import harness, system, cable
+from harnice.utils import circuit_utils
+from harnice.lists import channel_map, signals_list
 
 def main():
     #========================================================
@@ -40,5 +42,29 @@ def main():
 
     md.append("```")
     path = docs_functions.harnice_dir() / "docs" / "fragments" / "_system_default_feature_tree.md"
+    path.parent.mkdir(parents=True, exist_ok=True)
+    path.write_text("".join(md), encoding="utf-8")
+
+    #========================================================
+    # MAPPING VOCABULARY
+    #========================================================
+    module_prefix = "mapping_vocabulary"
+
+    md = ['??? note "Mapping vocabulary"']
+
+    md.append(f"\n\n    **Cable:**")
+    md.append(f"\n\n    - {cable.documentation_description}\n\n")
+    md.append(f"\n\n    **Conductor:**")
+    md.append(f"\n\n    - {circuit_utils.conductor_documentation_description}\n\n")
+    md.append(f"\n\n    **Circuit:**")
+    md.append(f"\n\n    - {circuit_utils.circuit_documentation_description}\n\n")
+    md.append(f"\n\n    **Channel:**")
+    md.append(f"\n\n    - {channel_map.channel_documentation_description}\n\n")
+    md.append(f"\n\n    **Net:**")
+    md.append(f"\n\n    - A Kicad/eCad term that represents a common node of electrical signals. Used in Harnice to represent a physical harness build, that bounds connectors into one set.")
+    md.append(f"\n\n    **Signal:**")
+    md.append(f"\n\n    - {signals_list.signal_documentation_description}\n\n")
+
+    path = docs_functions.harnice_dir() / "docs" / "fragments" / "_mapping_vocabulary.md"
     path.parent.mkdir(parents=True, exist_ok=True)
     path.write_text("".join(md), encoding="utf-8")
